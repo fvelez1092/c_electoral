c6bf35e475cefc0ab6244186a7aeee8775841324
fernando velez <fvelez1092@gmail.com>
2023-07-18 11:25:32 -0500

    Correccion de las rutas

--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -1,10 +1,7 @@
 import { Router } from 'express'
 import { check } from 'express-validator'
 import {
-  getPreinscripcion,
-  createPerson,
   getPersons,
-  getPerson,
   updatePerson,
   getAssistants,
 } from '../controllers/auth_controller'
@@ -16,29 +13,29 @@ router.post(
   [check('cedula', 'Cedula is required').not().isEmpty(), validateFields],
   updatePerson
 )
-router.post(
-  '/getPerson',
-  [check('cedula', 'Cedula is required').not().isEmpty(), validateFields],
-  getPerson
-)
-router.post(
-  '/getPreinscripcion',
-  [check('cedula', 'Cedula is required').not().isEmpty(), validateFields],
-  getPreinscripcion
-)
+// router.post(
+//   '/getPerson',
+//   [check('cedula', 'Cedula is required').not().isEmpty(), validateFields],
+//   getPerson
+// )
+// router.post(
+//   '/getPreinscripcion',
+//   [check('cedula', 'Cedula is required').not().isEmpty(), validateFields],
+//   getPreinscripcion
+// )
 router.get('/getPersons', getPersons)
 router.get('/getAssistants', getAssistants)
 
-router.post(
-  '/createPerson',
-  [
-    check('cedula', 'Cedula is required').not().isEmpty(),
-    check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Email is required').isEmail(),
-    validateFields,
-  ],
-  createPerson
-)
+// router.post(
+//   '/createPerson',
+//   [
+//     check('cedula', 'Cedula is required').not().isEmpty(),
+//     check('name', 'Name is required').not().isEmpty(),
+//     check('email', 'Email is required').isEmail(),
+//     validateFields,
+//   ],
+//   createPerson
+// )
 
 // router.post('/login', [
 //     check('password', 'Password is empty').not().isEmpty(),
